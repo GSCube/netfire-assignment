795e6192c9414f9c10492e9d4d9953a85c2b1906
Grzegorz Stosio <0mcabra@gmail.com>
2024-03-12 01:24:48 +0100

    Add styled components theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import HeroPlayer from './components/HeroPlayer';
 import videoSrc from './assets/video/hero.mp4';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
 
 const backupUrl = 'https://www.pexels.com/video/birds-flying-on-the-sky-9206132';
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <header>Header</header>
       <HeroPlayer videoSrc={videoSrc} backupUrl={backupUrl}>
         <h1>Hero Video</h1>
       </HeroPlayer>
-    </>
+    </ThemeProvider>
   );
 };
 
---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    primary: '##ECECEC',
+    secondary: '#D9D9D9',
+    accent: '#CAE871',
+    transparent: 'rgba(3, 2, 2, 0.3)',
+  },
+  radius: {
+    medium: '10px',
+    large: '12px',
+  },
+};
+
+export default theme;
