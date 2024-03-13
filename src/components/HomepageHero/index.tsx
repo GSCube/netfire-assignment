24ac5cf49915626ce088684acf7be3c0793bfb58
Grzegorz Stosio <0mcabra@gmail.com>
2024-03-13 19:35:09 +0100

    Adjust Homepage hero

--- src/components/HomepageHero/index.tsx
+++ src/components/HomepageHero/index.tsx
@@ -1,14 +1,25 @@
 import HeroPlayer from '../HeroPlayer';
-import videoSrc from '../../assets/video/hero.mp4';
+import videoSrc from 'assets/video/hero.mp4';
 import React from 'react';
-import { Heading } from '../Typography';
+import { GradientText, Heading } from '../Typography';
+import styled from 'styled-components';
+import { ButtonCTA } from '../Buttons';
 
 const backupUrl = 'https://www.pexels.com/video/birds-flying-on-the-sky-9206132';
+
+const ButtonWrapper = styled.div`
+  margin-bottom: 40px;
+`;
 
 export const HomepageHero = () => {
   return (
     <HeroPlayer videoSrc={videoSrc} backupUrl={backupUrl}>
-      <Heading>Lorem Ipsum Dolor Sit amet</Heading>
+      <ButtonWrapper>
+        <ButtonCTA>LOREM IPSUM</ButtonCTA>
+      </ButtonWrapper>
+      <Heading>
+        <GradientText>Lorem Ipsum Dolor Sit amet</GradientText>
+      </Heading>
       <Heading>Donec cursus ligula diam, nec congue augue ultrices nec.</Heading>
     </HeroPlayer>
   );
