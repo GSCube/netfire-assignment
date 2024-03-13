de58baa25f54e3272bf5a5206cac0fcd31e5b3d0
Grzegorz Stosio <0mcabra@gmail.com>
2024-03-13 19:37:19 +0100

    Adjust App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import theme from './theme';
 import { HomepageHero } from './components/HomepageHero';
+import { GlobalStyle } from './globalStyles';
+import PromoBanner from './components/PromoBanner';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ContentSection from './components/ContentSection';
+import { AppContainer, MainContentContainer } from './components/Layout';
+import { Paragraph } from './components/Typography';
+
+export const HighlightedText = styled.span`
+  color: ${({ theme }) => theme.colors.promotionHighlight};
+`;
+
+const PromoBannerMessage = (
+  <Paragraph>
+    NEW CUSTOMERS GET 15% OFF! USE CODE <HighlightedText>WELCOME15</HighlightedText>
+  </Paragraph>
+);
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <header>Header</header>
-      <HomepageHero />
+      <GlobalStyle />
+
+      <AppContainer>
+        <PromoBanner message={PromoBannerMessage} />
+        <Header />
+        <HomepageHero />
+        <MainContentContainer>
+          <ContentSection />
+        </MainContentContainer>
+        <Footer />
+      </AppContainer>
     </ThemeProvider>
   );
 };
