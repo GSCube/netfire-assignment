d8da56e7dc8b081a7198f09342da3d633a648429
Grzegorz Stosio <0mcabra@gmail.com>
2024-03-13 23:16:21 +0100

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 import { HomepageHero } from './components/HomepageHero';
 import { GlobalStyle } from './globalStyles';
