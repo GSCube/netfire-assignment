397ea024d7003987fd9613754f6972bab61f7341
Grzegorz Stosio <0mcabra@gmail.com>
2024-03-13 19:32:49 +0100

    Add AccentBar

---
+++ src/components/AccentBar/index.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const AccentBar = styled.div`
+  background: ${({ theme }) => theme.colors.gradient};
+  width: 100vw;
+  display: block;
+  height: 10px;
+`;
